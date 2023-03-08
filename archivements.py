ae72bcb93089b9618ea887140e1fdd98c2643dc3
Heiko999 <84904473+Heiko999@users.noreply.github.com>
2023-03-08 12:41:08 +0100

    added archivement classes. need integration

---
+++ archivements.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+
+class archivement(ABC):
+    @abstractmethod
+    def __init__(self,points):
+        pass
+
+    @abstractmethod
+    def congrats():
+        pass
+
+class bigger_archivement(archivement):
+    @abstractmethod
+    def music_play():
+        pass
+
+class points_reached(archivement):
+    def __init__(self, points):
+        self.points = points
+
+    def congrats(self):
+        print("congrats you got %d points!" % self.points)
+
+class highscore_reached(bigger_archivement):
+    def __init__(self, points):
+        self.points = points
+
+    def congrats(self):
+        print("congrats you got %d points and broke the Highscore!" % self.points)
+
+    def music_play(self):
+        print("play musik") 
