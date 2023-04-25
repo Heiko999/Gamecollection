0bf714d24343f7911d838a9f197f5709a1b7e575
Heiko999 <84904473+Heiko999@users.noreply.github.com>
2023-04-25 22:52:12 +0200

    Add files via upload

--- testsnake.py
+++ testsnake.py
@@ -1,58 +1,55 @@
 import unittest
+from unittest.mock import Mock
 import pygame as pg
 from snake import SnakeGame
+from pyvirtualdisplay import Display
 
 
 class TestGame(unittest.TestCase):
-    
-    @classmethod
-    def setUpClass(cls):
-        pg.display.init()
-        pg.display.set_mode((1, 1), pg.NOFRAME)
-    
+
     def test_snake_moveright(self):
         game = SnakeGame()
         initial_position = game.snake.rect.center
-        pg.event.post(pg.event.Event(pg.KEYDOWN, {"key": pg.K_RIGHT}))  # move right
-        game.check_event()
-        game.update()
+        event = Mock(type=pg.KEYDOWN, key=pg.K_RIGHT)
+        game.snake.control(event)
+        game.snake.update()
         updated_position = game.snake.rect.center
         updated_direction = game.snake.direction
         self.assertNotEqual(initial_position, updated_position)
-        self.assertEqual(updated_direction, (50, 0))  # Check if direction changed to right
-    
+        self.assertEqual(updated_direction, (50, 0))  # Check if direction changed to right    
+
     def test_snake_moverleft(self):
         game = SnakeGame()
         initial_position = game.snake.rect.center
-        pg.event.post(pg.event.Event(pg.KEYDOWN, {"key": pg.K_LEFT}))  # move right
-        game.check_event()
-        game.update()
+        event = Mock(type=pg.KEYDOWN, key=pg.K_LEFT)
+        game.snake.control(event)
+        game.snake.update()
         updated_position = game.snake.rect.center
         updated_direction = game.snake.direction
         self.assertNotEqual(initial_position, updated_position)
-        self.assertEqual(updated_direction, (-50, 0))  # Check if direction changed to right
+        self.assertEqual(updated_direction, (-50, 0))  # Check if direction changed to left
 
     def test_snake_moveup(self):
         game = SnakeGame()
         initial_position = game.snake.rect.center
-        pg.event.post(pg.event.Event(pg.KEYDOWN, {"key": pg.K_UP}))  # move right
-        game.check_event()
-        game.update()
+        event = Mock(type=pg.KEYDOWN, key=pg.K_UP)
+        game.snake.control(event)
+        game.snake.update()
         updated_position = game.snake.rect.center
         updated_direction = game.snake.direction
         self.assertNotEqual(initial_position, updated_position)
-        self.assertEqual(updated_direction, (0, -50))  # Check if direction changed to right
+        self.assertEqual(updated_direction, (0, -50))  # Check if direction changed to up
 
     def test_snake_movedown(self):
         game = SnakeGame()
         initial_position = game.snake.rect.center
-        pg.event.post(pg.event.Event(pg.KEYDOWN, {"key": pg.K_DOWN}))  # move right
-        game.check_event()
-        game.update()
+        event = Mock(type=pg.KEYDOWN, key=pg.K_DOWN)
+        game.snake.control(event)
+        game.snake.update()
         updated_position = game.snake.rect.center
         updated_direction = game.snake.direction
         self.assertNotEqual(initial_position, updated_position)
-        self.assertEqual(updated_direction, (0, 50))  # Check if direction changed to right
+        self.assertEqual(updated_direction, (0, 50))  # Check if direction changed to down
 
         
 
